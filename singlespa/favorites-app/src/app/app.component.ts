6d938d0b9d884f08335b2a542b7dd9515d32c599
Val <valexx55@gmail.com>
2026-03-10 11:23:33 +0100

    single spa TODO local delete

--- singlespa/favorites-app/src/app/app.component.ts
+++ singlespa/favorites-app/src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+
+interface FavoriteItem {
+  id: number;
+  title: string;
+  summary: string;
+  image_url: string;
+  url: string;
+}
+
+interface FavoriteMessage {
+  type: string;
+  payload: FavoriteItem;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'favorites-app';
+export class AppComponent implements OnInit, OnDestroy {
+  favorites: FavoriteItem[] = [];
+  channel = new BroadcastChannel('mf-channel');
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit(): void {
+    const key = 'mf-favorites';
+
+    const saved = localStorage.getItem(key);
+
+    if (saved) {
+      this.favorites = JSON.parse(saved);
+    }
+
+    this.channel.onmessage = (event: MessageEvent<FavoriteMessage>) => {
+      this.ngZone.run(() => {
+        if (event.data.type === 'news:add-favorite') {
+          const item = event.data.payload;
+
+          if (!this.favorites.some((f) => f.id === item.id)) {
+            this.favorites.push(item);
+            localStorage.setItem('mf-favorites', JSON.stringify(this.favorites));
+          }
+
+          console.log('Favorito recibido', item);
+          console.log('Favorites actualizados', this.favorites);
+        }
+      });
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.channel.close();
+  }
+
+  removeFavorite(id: number): void {
+    this.favorites = this.favorites.filter((f) => f.id !== id);
+    //TODO: eliminar favoritos del navegador
+  }
 }
