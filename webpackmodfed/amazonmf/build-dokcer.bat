8cfce7ea47181e15b841ab44f27905a0d500a767
Val <valexx55@gmail.com>
2026-03-13 07:55:28 +0100

    dpconfig

---
+++ webpackmodfed/amazonmf/build-dokcer.bat
@@ -0,0 +1,19 @@
+@echo off
+
+cd /d %~dp0 #ve al directorio donde está este script
+
+echo Construyendo shell...
+docker build -t imf/shell:latest -f projects/shell/Dockerfile .
+
+echo Construyendo productos...
+docker build -t imf/productos:latest -f projects/productos/Dockerfile .
+
+echo Construyendo carrito...
+docker build -t imf/carrito:latest -f projects/carrito/Dockerfile .
+
+echo Construyendo perfil...
+docker build -t imf/perfil:latest -f projects/perfil/Dockerfile .
+
+echo.
+echo Build completado
+pause
